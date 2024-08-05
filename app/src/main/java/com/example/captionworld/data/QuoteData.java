dcd43aa523c37479dd4bd716fb2e2e8669177b50
crnisarga <nisargagowdacr@gmail.com>
2024-08-05 21:26:11 +0530

    Basic Code

---
+++ app/src/main/java/com/example/captionworld/data/QuoteData.java
@@ -0,0 +1,14 @@
+package com.example.captionworld.data;
+
+import com.example.captionworld.modal.Quote;
+
+import java.util.ArrayList;
+
+public class QuoteData {
+
+    ArrayList<Quote> quotearray = new ArrayList<>();
+
+    public void getQuote() {
+
+    }
+}
---
+++ app/src/main/java/com/example/captionworld/modal/Quote.java
@@ -0,0 +1,23 @@
+package com.example.captionworld.modal;
+
+public class Quote {
+
+    private String quote;
+    private String scenario;
+
+    public String getScenario() {
+        return scenario;
+    }
+
+    public void setScenario(String scenario) {
+        this.scenario = scenario;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
