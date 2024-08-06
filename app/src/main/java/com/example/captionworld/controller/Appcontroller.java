3e3de8014c7cf9674425280100b679fb0d34ec4c
crnisarga <nisargagowdacr@gmail.com>
2024-08-06 21:45:21 +0530

    Basic Code

--- app/src/main/java/com/example/captionworld/controller/Appcontroller.java
+++ app/src/main/java/com/example/captionworld/controller/Appcontroller.java
@@ -1,32 +1,64 @@
 package com.example.captionworld.controller;
 
 import android.app.Application;
+import android.text.TextUtils;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 public class Appcontroller extends Application {
+    // since it extends Application, hence it's life cycle will be throughout the whole App
+
     public static final String TAG = Appcontroller.class.getSimpleName();
-    private static Appcontroller mInstance;
+    // TAG is used to determine which Activity has logged in information in the logcat
+
     private RequestQueue mRequestQueue;
 
-    public static synchronized Appcontroller getInstance() {return mInstance;}
+    private static Appcontroller mInstance;
+    // so that we have only one request queue & one image loader
 
     @Override
     public void onCreate() {
-        super.onCreate();
+        super.onCreate(); // calling the constructor of the super class i.e Application is created
+        /// before any service/Activity
         mInstance = this;
+    }
+
+    public static synchronized Appcontroller getInstance() {
+        return mInstance;
     }
 
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
             mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+            // setting up a new request queue
+            // getApplicationContext() returns the context for the entire application ( the
+            // processes all the Activities are running inside of).
         }
+        // Volley works with disk based cache i.e it caches files directly into the hard disk in the
+        // specified directory
+
+        // if we make a request, volley goes through the cache. If there's a cache hit then it
+        // returns with that request.
+        // in case of a cache miss, it makes an HTTP call & parses the response
+        // & put that entry into the cache
+
         return mRequestQueue;
     }
 
-    public <T> void addToRequestQueue(Request<T> req, String tag) {
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+        // if we make a request, volley goes through the cache. If there's a cache hit then it
+        // returns with that request.
+        // in case of a cache miss, it makes an HTTP call & parses the response
+        // & put that entry into the cache
+    }
 
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
     }
 }
--- app/src/main/java/com/example/captionworld/data/QuoteData.java
+++ app/src/main/java/com/example/captionworld/data/QuoteData.java
@@ -1,6 +1,13 @@
 package com.example.captionworld.data;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.captionworld.controller.Appcontroller;
 import com.example.captionworld.modal.Quote;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -10,5 +17,18 @@ public class QuoteData {
 
     public void getQuote() {
         String url = "https://github.com/crnisarga/CaptionWorld/blob/master/Caption.txt";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
     }
 }
